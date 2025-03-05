195566aab1164395f08eda6019e664a803a99297
Ahmed <ahmedsayedhamed38@gmail.com>
2025-03-05 22:13:09 +0200

    asdf

--- src/app/components/Home/SecondSection.jsx
+++ src/app/components/Home/SecondSection.jsx
@@ -6,14 +6,9 @@ import Image from "next/image";
 
 function SecondSection() {
 
-
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-
-
-
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
     <div className="bg-blue-50  ">
@@ -24,75 +19,70 @@ const scrollToTop = () => {
         </p>
       </div>
 
-<div>
-  <Image
-    src={"/images/sectionOne.png"}
-    alt="logo"
-    width={1000}
-    height={1000}
-    className="mx-auto"
-  />
-</div>
-
-
-
-
+      <div>
+        <Image
+          src={"/images/sectionOne.png"}
+          alt="logo"
+          width={1000}
+          height={1000}
+          className="mx-auto"
+        />
+      </div>
 
       <div className="flex flex-wrap md:flex-nowrap px-4  items-center justify-center gap-10 mt-16  py-10 ">
-      <div className="bg-white relative shadow rounded-lg text-right px-4 pl-6 py-8 w-full md:w-[380px] md:h-[340px] flex flex-col justify-between">
-  <div>
-    <h2 className="flex items-top gap-2 font-semibold text-xl justify-end">
-      اسال محامي مجانا
-      <span className="bg-[#16498C] h-fit px-1 py-1 rounded-2xl">
-        <MdQuestionMark className="text-white" />
-      </span>
-    </h2>
+        <div className="bg-white relative shadow rounded-lg text-right px-4 pl-6 py-8 w-full md:w-[380px] lg:h-[340px] md:h-[370px] flex flex-col">
+          <div className="flex-grow">
+            <h2 className="flex items-top gap-2 font-semibold text-xl justify-end">
+              اسال محامي مجانا
+              <span className="bg-[#16498C] h-fit px-1 py-1 rounded-2xl">
+                <MdQuestionMark className="text-white" />
+              </span>
+            </h2>
 
-    <ul className="flex flex-col gap-2 py-4 pr-10 text-gray-600">
-      <li>اسال محامي عن اي مشكلتك القانونية</li>
-      <li>احصل على إجابة سريعة و دقيقة</li>
-      <li>خدمة متاحة لجميع القضايا</li>
-    </ul>
-  </div>
+            <ul className="flex flex-col gap-2 py-4 pr-10 text-gray-600">
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> اسال محامي عن اي مشكلتك القانونية</li>
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> احصل على إجابة سريعة و دقيقة</li>
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> خدمة متاحة لجميع القضايا</li>
+            </ul>
+          </div>
 
-  <Link href="/Askquestion">
-    <button className="mt-4 flex items-center justify-center gap-2 hover:bg-blue-50 text-lg border-2 w-full py-3 rounded-md border-gray-400 hover:border-gray-600">
-      استشارة مجانية
-      
-    </button>
-  </Link>
-</div>
+          <Link href="/Askquestion" className="mt-auto w-full">
+            <button className="w-full py-3 rounded-md border-2 border-gray-400 hover:border-gray-600 hover:bg-blue-50 text-lg">
+              استشارة مجانية
+            </button>
+          </Link>
+        </div>
 
+        <div className="bg-white shadow rounded-lg text-right px-4 pl-6 py-8 w-full md:w-[380px] lg:h-[340px] md:h-[370px] flex flex-col">
+          <div className="flex-grow">
+            <h2 className="flex items-top gap-2 font-semibold text-lg justify-end">
+              ابحث عن محامي متخصص في قضيتك
+              <span className="bg-[#16498C] h-fit px-1 py-1 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-4 text-white"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </h2>
 
-        <div className="bg-white shadow rounded-lg text-right px-4 pl-6  py-8 w-full   md:w-[380px] md:h-[340px]">
-          <h2 className="flex items-top gap-2  font-semibold text-lg justify-end">
-          ابحث عن محامي متخصص في قضيتك
-            <span className="bg-[#16498C] h-fit px-1 py-1 rounded-2xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-4 text-white"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </h2>
+            <ul className="flex flex-col gap-2 py-4 pr-9 text-gray-600">
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> جميع المحامين في بشارة متخصصين في مجالات قانونية حسب خبراتهم و مؤهلاتهم </li>
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> جميع المراجعات في بشارة موثقة</li>
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> المحامين المعتمدين</li>
+              <li dir="rtl" className="flex items-center gap-2"><span>*</span> جميع المحامين مرخصين من وزارة العدل</li>
+            </ul>
+          </div>
 
-          <ul className="flex flex-col gap-2 py-4 pr-9 text-gray-600">
-            <li>جميع المحامين في بشارة متخصصين في مجالات قانونية حسب خبراتهم و مؤهلاتهم</li>
-            <li>جميع المراجعات في بشارة موثقة</li>
-            <li>المحامين المعتمدين</li>
-            <li>جميع المحامين مرخصين من وزارة العدل</li>
-          </ul>
-
-          <button onClick={scrollToTop} className="mt-4  flex items-center justify-center gap-1 hover:bg-blue-50  text-lg border-2 w-full py-3 rounded-md border-gray-400 hover:border-gray-600">
-          ابدأ في البحث
-           
+          <button onClick={scrollToTop} className="w-full py-3 rounded-md border-2 border-gray-400 hover:border-gray-600 hover:bg-blue-50 text-lg mt-auto">
+            ابدأ في البحث
           </button>
         </div>
       </div>
